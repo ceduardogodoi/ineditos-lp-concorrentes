b91ebdc12a395f17e7d7ba77a06f618b0c900d06
Carlos Godoi <eduardo.godoi@i-cherry.com.br>
2019-06-03 17:53:22 -0300

    Adiciona cards da seção 'Conheça o Paraná Banco'

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation()
+$(document).foundation();
+
+$(document).ready(() => {
+    $('.slick-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: false,
+        className: 'center',
+        centerMode: true,
+        slidesToShow: 1,
+        centerPadding: '30px'
+    });
+});
