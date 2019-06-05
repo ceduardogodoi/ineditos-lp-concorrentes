644bcb65b703aabd7f2653e264b15bbf21ca03ea
Carlos Godoi <eduardo.godoi@i-cherry.com.br>
2019-06-05 17:09:31 -0300

    Ajusta carrosséis na resolução 768px de largura

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 $(document).foundation();
 
 $(document).ready(() => {
-    $('.slick-slider').slick({
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: false,
-        className: 'center',
-        centerMode: true,
-        slidesToShow: 1,
-        centerPadding: '30px'
-    });
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        $('.slick-slider').slick({
+            dots: false,
+            infinite: false,
+            speed: 500,
+            slidesToScroll: 1,
+            arrows: false,
+            autoplay: false,
+            className: 'center',
+            centerMode: true,
+            slidesToShow: 1,
+            centerPadding: '30px'
+        });
+    }
+    // else {
+    //     const slickers = document.querySelectorAll('.slick-slider');
+    //     slickers.forEach(slicker => slicker.classList.remove('slick-slider'));
+    // }
 });
